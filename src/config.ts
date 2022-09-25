db1c1036284afa0867ec33507ba18eb288adcc77
chris <chrissantosproduction@gmail.com>
2022-09-25 14:52:27 -0400

    Adding database

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,14 @@ const config = {
     jwt: {
         secret: '{{JWT_SECRET}}' 
     },
+    database: {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        name: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    },
     cors: {
         origins: 'http://localhost:3000' 
     }
